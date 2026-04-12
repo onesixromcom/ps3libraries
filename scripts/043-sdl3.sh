980941f1397e85fc8d3562b890b8d73154663526
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2026-04-12 22:55:02 +0300

    Add SDL3 scripts and patches, cmake files for ps3

---
+++ scripts/043-sdl3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+# sdl3.sh by 16rom.com
+
+BUILD_NAME="SDL3"
+BUILD_DIR="build-ppc"
+
+# Copy make file.
+if [ ! -f "$PS3DEV/share/ps3dev.cmake" ]; then
+	mkdir -p "$PS3DEV/share"
+	cp ../depends/ps3dev.cmake  "$PS3DEV/share/ps3dev.cmake"
+fi
+
+# Download if it's not there.
+if [ ! -d "$BUILD_NAME" ]; then
+	## Download the source code.
+	wget https://github.com/onesixromcom/SDL/tarball/ps3 -O "$BUILD_NAME.tar.gz"
+	## Unpack the source code.
+	rm -Rf "$BUILD_NAME"
+	mkdir "$BUILD_NAME" && tar --strip-components=1 --directory="$BUILD_NAME" -xvzf "$BUILD_NAME".tar.gz
+fi
+
+## Create the build directory.
+cd "$BUILD_NAME"
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+echo "Building for PPC..."
+
+cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE="../../depends/ps3build.cmake" -DCMAKE_INSTALL_PREFIX="$PS3DEV/portlibs/ppu" -DCMAKE_PREFIX_PATH="$PS3DEV/portlibs/ppu" -DSDL_TESTS=OFF  -DSDL_EXAMPLES=OFF -DSDL_TEST_LIBRARY=OFF -DSDL_STATIC=ON \  ..
+cmake --build . 
+cmake --install .
---
+++ scripts/044-sdl3_mixer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+# sdl3_mixer.sh by 16rom.com
+
+BUILD_NAME="SDL3_mixer"
+BUILD_DIR="build-ppc"
+
+# Download if it's not there.
+if [ ! -d "$DIRECTORY" ]; then
+	## Download the source code.
+	wget https://github.com/libsdl-org/SDL_mixer/releases/download/release-3.2.0/SDL3_mixer-3.2.0.tar.gz
+	## Unpack the source code.
+	rm -Rf "$BUILD_NAME"
+	mkdir "$BUILD_NAME" && tar --strip-components=1 --directory="$BUILD_NAME" -xvzf SDL3_mixer-3.2.0.tar.gz
+fi
+
+## Create the build directory.
+cd "$BUILD_NAME"
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+echo "Building for PPC..."
+
+cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE="../../depends/ps3build.cmake"  -DCMAKE_INSTALL_PREFIX="$PS3DEV/portlibs/ppu" -DCMAKE_PREFIX_PATH="$PS3DEV/portlibs/ppu" -DBUILD_SHARED_LIBS=OFF -DSDLMIXER_TESTS=OFF -DSDLMIXER_EXAMPLES=OFF -DSDL_STATIC=ON  -DSDLMIXER_OPUS=OFF -DSDLMIXER_VORBIS_VORBISFILE=OFF -DSDLMIXER_WAVPACK=OFF -DSDLMIXER_OGG=OFF -DSDLMIXER_FLAC=OFF -DSDLMIXER_MP3_MPG123=OFF -DSDLMIXER_GME=OFF -DSDLMIXER_MOD_XMP=OFF -DSDLMIXER_MIDI_TIMIDITY=OFF ..
+cmake --build . 
+cmake --install .
